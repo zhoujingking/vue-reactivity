8483a26550ea18c80d6a608abedb75a99ffb3134
zhoujingking <85977400+zhoujingking@users.noreply.github.com>
2022-08-20 21:41:34 +0800

    problem: pure javascript behavior

---
+++ src/index.js
@@ -0,0 +1,9 @@
+let price = 5;
+let quanity = 2;
+let total = price * quanity;
+
+console.log(`total is ${total}`); // 10
+
+price = 20;
+
+console.log(`total is ${total}`); // total is not automatically updated!!
