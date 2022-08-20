ce423da9ad11891f8f1ffef633ae8e06149e3a75
zhoujingking <85977400+zhoujingking@users.noreply.github.com>
2022-08-20 22:23:32 +0800

    Reactivity of object which has multiple props, and each prop has their own dep

--- src/index.js
+++ src/index.js
@@ -1,35 +1,59 @@
-let price = 5;
-let quanity = 2;
+/*
+  previous version is one property version.
+  Here, we to for object which has multiple properties, and
+  each property has their own dep.
+*/
+
+const product = {
+  price: 5,
+  quanity: 2
+};
+
+const depsMap = new Map();
+
 let total = 0;
-// steps
-// 1. allocate a storage (dep)
-// 2. save the effect into storage (dep)
-// 3. run (re-run) teh effect later
 
-// the dependency of total
-let dep = new Set(); // to store our effects
-
-// save this effect into storage => above set
-let effect = () => {
-  total = price * quanity;
+const effect = () => {
+  total = product.price * product.quanity;
 };
 
 // save the effect into storage
-const track = () => {
+const track = (key) => {
+  let dep = depsMap.get(key);
+  if (!dep) {
+    // if no dep, let's create one
+    depsMap.set(key, (dep = new Set()));
+  }
   dep.add(effect);
 };
 
 // to re-run the effect
-const trigger = () => {
-  dep.forEach(effect => effect());
+const trigger = (key) => {
+  const dep = depsMap.get(key);
+  if (dep) {
+    dep.forEach(effect => effect())
+  }
 };
 
-track();
 
-trigger();
+// const keyToTrack = 'quantity';
+// track(keyToTrack);
+// trigger(keyToTrack);
+// console.log(`total is ${total}`);
+
+// // if some dependency changes, re-run effect
+// product.price = 20;
+// trigger(keyToTrack);
+// console.log(`total is ${total}`);
+
+Object.keys(product).forEach(key => {
+  track(key);
+  trigger(key);
+});
 console.log(`total is ${total}`);
-
-// if some dependency changes, re-run effect
-price = 20;
-trigger();
-console.log(`total is ${total}`); // total is not automatically updated!!
+product.quanity = 20;
+trigger('quanity');
+console.log(`total is ${total}`);
+product.price = 13;
+trigger('price');
+console.log(`total is ${total}`);
